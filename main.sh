cd92300e7a52a809f086eb2c9149a49bc08fbb8d
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-09-26 20:43:16 +0900

    update

--- main.sh
+++ main.sh
@@ -1,23 +1,21 @@
 DESKTOP="$HOME/Desktop"
 TARGET="$DESKTOP/articles"
 
-ls $TARGET | while read line; do
-    if [[ $line = *.md ]]; then
-        target="$TARGET/$line"
-        without_file_name=$(echo $line | cut -d '.' -f1)
-        without_extension_path=$(echo $target | cut -d '.' -f1)
+for line in $(ls $TARGET | grep '.md$'); do
+    target="$TARGET/$line"
+    without_file_name=$(echo $line | cut -d '.' -f1)
+    without_extension_path=$(echo $target | cut -d '.' -f1)
 
-        awk "{system(\"md-to-pdf $target\");}"
-        icon=$(cat $target \
-        | grep '^icon:'    \
-        | cut -d ':' -f2   \
-        | awk '{gsub(/"/, "", $0); gsub(/ /, "", $0); print($0);}')
-        mv $without_extension_path.pdf $DESKTOP
+    md-to-pdf $target
+    icon=$(cat $target \
+    | grep '^icon:'    \
+    | cut -d ':' -f2   \
+    | awk '{gsub(/"/, "", $0); gsub(/ /, "", $0); print($0);}')
+    mv $without_extension_path.pdf $DESKTOP
 
-        if [ -z $icon ]; then
-            ic conv $DESKTOP/$without_file_name.pdf@pdf
-        else
-            ic conv $DESKTOP/$without_file_name.pdf@$icon
-        fi
+    if [ -z $icon ]; then
+        ic conv $DESKTOP/$without_file_name.pdf@pdf
+    else
+        ic conv $DESKTOP/$without_file_name.pdf@$icon
     fi
 done
