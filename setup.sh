5dd82b67bd6b0b7eb68adcd407009c3506b4e870
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-09-26 22:55:12 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
+currentDirectory=$(cd $(dirname $0);pwd)
+echo 'export PATH="$PATH:'$currentDirectory\" >> ~/.zshrc
+source ~/.zshrc
+sudo chmod 755 $currentDirectory/artima
+
 npm install --global md-to-pdf @marp-team/marp-cli
